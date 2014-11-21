36c486a3f23c8cf4a28e603d12df5944981530b3
nmandel <nrmandel@ucdavis.edu>
2014-11-21 14:41:46 -0800

    set up the database

--- server/schema.sql
+++ server/schema.sql
@@ -3,13 +3,12 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
-/* Create other tables and define schemas for them here! */
-
-
-
+                      username VARCHAR(100),
+                      message VARCHAR(160),
+                      createdAt DATE,
+                      roomName VARCHAR(100),
+                      ID int(11) NOT NULL auto_increment, PRIMARY KEY (ID)
+                      );
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < schema.sql
